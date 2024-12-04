91b955423d1aaec755a293c57d04ca47bb2a4da6
Max Doronin <maxim.doronin@gmail.com>
2024-12-04 00:12:09 +0000

    Removed unnecessary tests in day3

--- Y2024/day3.py
+++ Y2024/day3.py
@@ -10,32 +10,31 @@ class DayXSolver(Solver):
         for line in self.input:
             i = 0
             while i < len(line) - 3:
-                if line[i:i+len("mul")] == "mul":
-                    if line[i+3] == "(":
-                        j = i + 4
-                        k = 0
-                        while line[j + k] != "," and k < 4 and line[j + k].isdigit():
-                            k += 1
-                        if line[j + k] == "," and 0 < k < 4 and line[j + k - 1].isdigit():
-                            x = int(line[j:j + k])
-                            i = j + k
-                        else:
-                            i = j + k
-                            continue
-                        if line[j + k] != ",":
-                            i = j + k
-                            continue
-                        j = i + 1
-                        k = 0
-                        while line[j + k] != ")" and k < 4 and line[j + k].isdigit():
-                            k += 1
-                        if line[j + k] == ")" and 0 < k < 4 and line[j + k - 1].isdigit():
-                            y = int(line[j:j + k])
-                        else:
-                            i = j + k
-                            continue
-                        result += x * y
+                if line[i:i + len("mul(")] == "mul(":
+                    j = i + 4
+                    k = 0
+                    while line[j + k] != "," and k < 4 and line[j + k].isdigit():
+                        k += 1
+                    if line[j + k] == "," and 0 < k < 4 and line[j + k - 1].isdigit():
+                        x = int(line[j:j + k])
                         i = j + k
+                    else:
+                        i = j + k
+                        continue
+                    if line[j + k] != ",":
+                        i = j + k
+                        continue
+                    j = i + 1
+                    k = 0
+                    while line[j + k] != ")" and k < 4 and line[j + k].isdigit():
+                        k += 1
+                    if line[j + k] == ")" and 0 < k < 4 and line[j + k - 1].isdigit():
+                        y = int(line[j:j + k])
+                    else:
+                        i = j + k
+                        continue
+                    result += x * y
+                    i = j + k
                 i += 1
         return result
 
@@ -46,37 +45,36 @@ class DayXSolver(Solver):
         for line in self.input:
             i = 0
             while i < len(line) - 3:
-                if line[i:i+len("do()")] == "do()":
+                if line[i:i + len("do()")] == "do()":
                     do = True
-                if line[i:i+len("don't()")] == "don't()":
+                if line[i:i + len("don't()")] == "don't()":
                     do = False
-                if line[i:i+len("mul")] == "mul":
-                    if line[i+3] == "(":
-                        j = i + 4
-                        k = 0
-                        while line[j + k] != "," and k < 4 and line[j + k].isdigit():
-                            k += 1
-                        if line[j + k] == "," and 0 < k < 4 and line[j + k - 1].isdigit():
-                            x = int(line[j:j + k])
-                            i = j + k
-                        else:
-                            i = j + k
-                            continue
-                        if line[j + k] != ",":
-                            i = j + k
-                            continue
-                        j = i + 1
-                        k = 0
-                        while line[j + k] != ")" and k < 4 and line[j + k].isdigit():
-                            k += 1
-                        if line[j + k] == ")" and 0 < k < 4 and line[j + k - 1].isdigit():
-                            y = int(line[j:j + k])
-                        else:
-                            i = j + k
-                            continue
-                        if do:
-                            result += x * y
+                if line[i:i + len("mul(")] == "mul(":
+                    j = i + 4
+                    k = 0
+                    while line[j + k] != "," and k < 4 and line[j + k].isdigit():
+                        k += 1
+                    if line[j + k] == "," and 0 < k < 4 and line[j + k - 1].isdigit():
+                        x = int(line[j:j + k])
                         i = j + k
+                    else:
+                        i = j + k
+                        continue
+                    if line[j + k] != ",":
+                        i = j + k
+                        continue
+                    j = i + 1
+                    k = 0
+                    while line[j + k] != ")" and k < 4 and line[j + k].isdigit():
+                        k += 1
+                    if line[j + k] == ")" and 0 < k < 4 and line[j + k - 1].isdigit():
+                        y = int(line[j:j + k])
+                    else:
+                        i = j + k
+                        continue
+                    if do:
+                        result += x * y
+                    i = j + k
                 i += 1
         return result
     
