142c6b9425ea724dc9fe7c7109bdc35e8d36d5dd
Max Doronin <maxim.doronin@gmail.com>
2024-12-05 18:10:06 +0000

    Prep for day 6.

---
+++ Y2024/tests/test_day6.py
@@ -0,0 +1,21 @@
+import unittest
+from Y2024.day6 import DayXSolver
+
+class TestDayXSolver(unittest.TestCase):
+    def setUp(self):
+        self.test_input1 = [
+            ""
+        ]
+
+    def test_first_problem(self):
+        solver = DayXSolver(None, None, None, self.test_input1)
+        result = solver.first_problem()
+        self.assertEqual(result, 0)
+
+    def test_second_problem(self):
+        solver = DayXSolver(None, None, None, self.test_input1)
+        result = solver.second_problem()
+        self.assertEqual(result, 0)
+
+if __name__ == '__main__':
+    unittest.main()
