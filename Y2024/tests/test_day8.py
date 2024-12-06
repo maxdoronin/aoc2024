000627ce8cd177131a5e4f1b8cc0a30f99ee6596
Max Doronin <maxim.doronin@gmail.com>
2024-12-06 14:21:00 +0000

    Day 8 solved.

---
+++ Y2024/tests/test_day8.py
@@ -0,0 +1,43 @@
+import unittest
+from Y2024.day8 import DayXSolver
+
+class TestDayXSolver(unittest.TestCase):
+    def setUp(self):
+        self.test_input1 = [
+            "............",
+            "........0...",
+            ".....0......",
+            ".......0....",
+            "....0.......",
+            "......A.....",
+            "............",
+            "............",
+            "........A...",
+            ".........A..",
+            "............",
+            "............"
+        ]
+        self.test_input2 = [
+            "T...........",
+            "...T....0...",
+            ".T...0......",
+            ".......0....",
+            "....0.......",
+            "......A.....",
+            "............",
+            "............",
+            "........A...",
+            ".........A..",
+            "............",
+            "............"
+        ]
+    def test_first_problem(self):
+        solver = DayXSolver(None, None, None, self.test_input1)
+        self.assertEqual(solver.first_problem(), 14)
+
+    def test_second_problem(self):
+        solver = DayXSolver(None, None, None, self.test_input1)
+        self.assertEqual(solver.second_problem(), 34)
+
+if __name__ == '__main__':
+    unittest.main()
