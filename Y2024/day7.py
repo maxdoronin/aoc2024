6c11eda1ab758b7583d487c69830c87a19b8ef56
Max Doronin <max.doronin@gmail.com>
2024-12-06 09:54:36 +0000

    Day 7 solved using dfs.

--- Y2024/day7.py
+++ Y2024/day7.py
@@ -3,14 +3,48 @@ from Solver import Solver
 class DayXSolver(Solver):
     def __init__(self, request, year, day, input=None):
         super().__init__(request, year, day, input)
+        self.calibration_values = {}
+        for line in self.input:
+            target_s, operands_s = line.split(":")
+            target = int(target_s)
+            operands = [int(x) for x in operands_s.strip().split(" ")]
+            self.calibration_values[target] = operands
 
+    def dfs1 (self, target, i=0, c_target=0):
+        operands = self.calibration_values[target]
+        if c_target > target:
+            return False
+        if i == len(operands):
+            return c_target == target
+        
+        return (self.dfs1 (target, i + 1, c_target + operands[i]) or
+                self.dfs1 (target, i + 1, c_target * operands[i]))
+
+    def dfs2 (self, target, i=0, c_target=0):
+        operands = self.calibration_values[target]
+        if c_target > target:
+            return False
+        if i == len(operands):
+            return c_target == target
+        
+        return (self.dfs2 (target, i + 1, c_target + operands[i]) or
+                self.dfs2 (target, i + 1, c_target * operands[i]) or
+                self.dfs2 (target, i + 1, int(str(c_target) + str(operands[i]))))
+
+    
     def first_problem(self):
         result = 0
+        for target in self.calibration_values.keys():
+            if self.dfs1(target):
+                result += target
         return result
 
     def second_problem(self):
         result = 0
-        return len(result)
+        for target in self.calibration_values.keys():
+           if self.dfs2(target):
+               result += target
+        return result
 
 def process(request, year, day):
     solver = DayXSolver(request, year, day)
--- Y2024/tests/test_day7.py
+++ Y2024/tests/test_day7.py
@@ -1,30 +1,42 @@
 import unittest
-from Y2024.day6 import DayXSolver
+from Y2024.day7 import DayXSolver
 
 class TestDayXSolver(unittest.TestCase):
     def setUp(self):
         self.test_input1 = [
-            "....#.....",
-            ".........#",
-            "..........",
-            "..#.......",
-            ".......#..",
-            "..........",
-            ".#..^.....",
-            "........#.",
-            "#.........",
-            "......#..."
+            "190: 10 19",
+            "3267: 81 40 27",
+            "83: 17 5",
+            "156: 15 6",
+            "7290: 6 8 6 15",
+            "161011: 16 10 13",
+            "192: 17 8 14",
+            "21037: 9 7 18 13",
+            "292: 11 6 16 20"
         ]
 
     def test_first_problem(self):
         solver = DayXSolver(None, None, None, self.test_input1)
-        result = solver.first_problem()
-        self.assertEqual(result, 41)
+        self.assertEqual(solver.dfs1(190), True)
+        self.assertEqual(solver.dfs1(3267), True)
+        self.assertEqual(solver.dfs1(292), True)
+        self.assertEqual(solver.dfs1(161011), False)
+        self.assertEqual(solver.dfs1(21037), False)
+        self.assertEqual(solver.dfs1(83), False)
+        self.assertEqual(solver.first_problem(), 3749)
 
     def test_second_problem(self):
         solver = DayXSolver(None, None, None, self.test_input1)
-        result = solver.second_problem()
-        self.assertEqual(result, 6)
+        self.assertEqual(solver.dfs2(156), True)
+        self.assertEqual(solver.dfs2(7290), True)
+        self.assertEqual(solver.dfs2(192), True)
+        self.assertEqual(solver.dfs1(190), True)
+        self.assertEqual(solver.dfs1(3267), True)
+        self.assertEqual(solver.dfs1(292), True)
+        self.assertEqual(solver.dfs2(21037), False)
+        self.assertEqual(solver.dfs2(83), False)
+        self.assertEqual(solver.dfs2(161011), False)
+        self.assertEqual(solver.second_problem(), 11387)
 
 if __name__ == '__main__':
     unittest.main()
