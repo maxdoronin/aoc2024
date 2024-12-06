3ecb5f04fdb35a5fd07a03a664ca53ba64071c51
Max Doronin <maxim.doronin@gmail.com>
2024-12-06 07:39:33 +0000

    Day 7 prep

---
+++ Y2024/day7.py
@@ -0,0 +1,18 @@
+from Solver import Solver
+import copy
+
+class DayXSolver(Solver):
+    def __init__(self, request, year, day, input=None):
+        super().__init__(request, year, day, input)
+
+    def first_problem(self):
+        result = 0
+        return result
+
+    def second_problem(self):
+        result = 0
+        return len(result)
+
+def process(request, year, day):
+    solver = DayXSolver(request, year, day)
+    return (solver.process())
