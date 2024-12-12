3d6307c4e11847f7a0764a6c1af928b0a3796238
Max Doronin <maxim.doronin@gmail.com>
2024-12-12 04:08:21 +0000

    Day 1 part 1 solved

--- Y2024/day12.py
+++ Y2024/day12.py
@@ -3,9 +3,39 @@ from Solver import Solver
 class DayXSolver(Solver):
     def __init__(self, request, year, day, input=None):
         super().__init__(request, year, day, input)
+        self.grid = self.input
+    
+    def bfs1(self, r, c, grid, visited):
+        steps = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        area = 0
+        perimeter = 0
+        queue = [(r, c)]
+        while queue:
+            r, c = queue.pop(0)
+            if (r, c) in visited:
+                continue
+            area += 1
+            visited.add((r, c))
+            plot_id = grid[r][c]
+            for step in steps:
+                nr, nc = r + step[0], c + step[1]
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                    if plot_id != grid[nr][nc]:
+                        perimeter += 1
+                    elif (nr, nc) not in visited:
+                        queue.append((nr, nc))
+                else:
+                    perimeter += 1
+        return (area, perimeter)
     
     def first_problem(self):
         result = 0
+        global_visited = set()
+        for r in range(len(self.grid)):
+            for c in range(len(self.grid[0])):
+                if (r, c) not in global_visited:
+                    a, p = self.bfs1(r, c, self.grid, global_visited)
+                    result += a * p
         return result
             
     def second_problem(self):
--- Y2024/tests/test_day12.py
+++ Y2024/tests/test_day12.py
@@ -4,11 +4,21 @@ from Y2024.day12 import DayXSolver
 class TestDayXSolver(unittest.TestCase):
     def setUp(self):
         self.test_input1 = [
+            "RRRRIICCFF",
+            "RRRRIICCCF",
+            "VVRRRCCFFF",
+            "VVRCCCJFFF",
+            "VVVVCJJCFE",
+            "VVIVCCJJEE",
+            "VVIIICJJEE",
+            "MIIIIIJJEE",
+            "MIIISIJEEE",
+            "MMMISSJEEE"
         ]
 
     def test_first_problem(self):
         solver = DayXSolver(None, None, None, self.test_input1)
-        self.assertEqual(solver.first_problem(), 0)
+        self.assertEqual(solver.first_problem(), 1930)
 
     def test_second_problem(self):
         solver = DayXSolver(None, None, None, self.test_input1)
