025a83249cf5c2e544cd20d0b57580a039ca43e8
Max Doronin <maxim.doronin@gmail.com>
2024-12-20 11:21:41 +0000

    Day 20, part 2 solved

--- Y2024/day20.py
+++ Y2024/day20.py
@@ -12,7 +12,6 @@ class DayXSolver(Solver):
                     break
 
     def chart_track(self, r, c, visited):
-        
         time = 0
         queue = [((r, c), 0)]
 
@@ -28,20 +27,26 @@ class DayXSolver(Solver):
                 queue.append(((r + dr, c + dc), time + 1))
         return
     
-    def find_cheats(self, visited):
+    def find_cheats(self, visited, max_cheat_len):
         cheats = {}
         for (r, c), time in visited.items():
-            for dir in self.directions:
-                r1 = r + dir[0] * 2
-                c1 = c + dir[1] * 2
-                if (r1, c1) in visited and visited[(r1, c1)] > time + 2:
-                    cheats[((r, c), (r1, c1))] = visited[(r1, c1)] - time - 2
+            for dr in range(-1 * max_cheat_len, max_cheat_len + 1):
+                for dc in range(-1 * max_cheat_len, max_cheat_len + 1):
+                    r1, c1 = r + dr, c + dc
+                    distance = abs(dr) + abs(dc)
+                    if (distance > max_cheat_len or
+                            distance <= 1 or
+                            (r1, c1) not in visited):
+                        continue
+                    cut_time = visited[(r1, c1)] - time - distance
+                    if cut_time > 0:
+                        cheats[((r, c), (r1, c1))] = cut_time
         return cheats
                        
     def first_problem(self):
         visited = {}
         self.chart_track(self.sr, self.sc, visited)
-        cheats = self.find_cheats(visited)
+        cheats = self.find_cheats(visited, 2)
 
         result = 0
         target = 100
@@ -51,7 +56,15 @@ class DayXSolver(Solver):
         return result
 
     def second_problem(self):
+        visited = {}
+        self.chart_track(self.sr, self.sc, visited)
+        cheats = self.find_cheats(visited, 20)
+
         result = 0
+        target = 100
+        for cut_time in sorted(cheats.values(), reverse=True):
+            if cut_time >= target:
+                result += 1
         return result
              
 def process(request, year, day):
--- Y2024/tests/test_day20.py
+++ Y2024/tests/test_day20.py
@@ -25,7 +25,7 @@ class TestDayXSolver(unittest.TestCase):
         solver = DayXSolver(None, None, None, self.test_input1)
         visited = {}
         solver.chart_track(solver.sr, solver.sc, visited)
-        cheats = solver.find_cheats(visited)
+        cheats = solver.find_cheats(visited, 2)
         cheat_times = sorted(cheats.values(), reverse=True)
         self.assertEqual(cheat_times.count(64), 1)
         self.assertEqual(cheat_times.count(40), 1)
@@ -37,6 +37,16 @@ class TestDayXSolver(unittest.TestCase):
 
     def test_second_problem(self):
         solver = DayXSolver(None, None, None, self.test_input1)
+        visited = {}
+        solver.chart_track(solver.sr, solver.sc, visited)
+        cheats = solver.find_cheats(visited, 20)
+        cheat_times = sorted(cheats.values(), reverse=True)
+        self.assertEqual(cheat_times.count(76), 3)
+        self.assertEqual(cheat_times.count(74), 4)
+        self.assertEqual(cheat_times.count(72), 22)
+        self.assertEqual(cheat_times.count(70), 12)
+        self.assertEqual(cheat_times.count(50), 32)
+        self.assertEqual(cheat_times.count(52), 31)
         self.assertEqual(solver.second_problem(), 0)
 
 if __name__ == '__main__':
